44961fdf52737f746ae9a5711a7ecdcea14060f4
Lucas Rigo <lucas.rigo@sap.com>
2020-12-08 11:08:28 -0300

    day 7 part 2

--- day7/handyHaversacks.js
+++ day7/handyHaversacks.js
@@ -8,6 +8,13 @@ dark olive bags contain 3 faded blue bags, 4 dotted black bags.
 vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
 faded blue bags contain no other bags.
 dotted black bags contain no other bags.`
+,`shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags.`
  ,puzzleInput
 ]
 
@@ -73,17 +80,55 @@ var part1 = function() {
 var part2 = function () {
 
   for (var i = 0; i < input.length; i++) {
-    var numberStrings = input[i].split(/\s+/)
-    var numbers = $.map(numberStrings, (val => {return Number(val)}))
+    var tupleStrings = input[i].split(/\n+/)
 
-    var result = 0
+    var graph = {}
+
+    $.each(tupleStrings, (idx,val)=>{
+      var words = val.split(/\s+/)
+      var mod = words[0]
+      var color = words[1]
+      graph[mod+color] = []
+      //bags 2
+      //contain 3
+      for (var w = 4; w < words.length; w++) {
+        var number = words[w]
+        if (number === 'no') { // no other bags.
+          break
+        }
+        var mod2 = words[++w]
+        var color2 = words[++w]
+        graph[mod+color].push({
+          num: Number(number),
+          bag: mod2+color2
+        })
+        if(words[++w].endsWith('.')) {
+          break
+        }
+      }
+    })
+    // console.log(graph)
+
+    var calcBags = function(node) {
+      if (graph[node].length === 0) {
+        return 1
+      } else {
+        var sum = 1 // the bag itself
+        $.each(graph[node], (idx,val)=>{
+          sum += val.num * calcBags(val.bag)
+        })
+        return sum
+      }
+    }
+
+    // console.log(hashbags)
+    var result = calcBags('shinygold')-1 // don't count the shiny gold bag
     // console.log(result)
     $('#part2').append(input[i])
       .append('<br>&emsp;')
       .append(result)
       .append('<br>')
   }
-
 }
 
 $(function (){
