a82aa4d3cc4566cdab3f918800867be268ee5cd5
Lucas Rigo <lucas.rigo@sap.com>
2020-12-09 07:10:28 -0300

    day 9 part 2

--- day9/encodingError.js
+++ day9/encodingError.js
@@ -19,34 +19,34 @@ var input = [
 277
 309
 576`
-,`20
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-21
-22
-23
-24
-25
-45
-65
-64`
+// ,`20
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
+// 7
+// 8
+// 9
+// 10
+// 11
+// 12
+// 13
+// 14
+// 15
+// 16
+// 17
+// 18
+// 19
+// 21
+// 22
+// 23
+// 24
+// 25
+// 45
+// 65
+// 64`
  ,puzzleInput
 ]
 
@@ -94,8 +94,55 @@ var part2 = function () {
   for (var i = 0; i < input.length; i++) {
     var numberStrings = input[i].split(/\s+/)
     var numbers = $.map(numberStrings, (val => {return Number(val)}))
+    var preamble = i === 0 ? 5 : 25
+    var errorNumber = -1
+    for (var n = preamble; n < numbers.length; n++) {
+      var next = numbers[n]
+      var check = false
+      for (var ni = n-preamble; ni < n; ni++) {
+        for (var nj = n-preamble+1; nj < n; nj++) {
+          if (ni===nj) {
+            continue
+          }
+          if (numbers[ni] + numbers[nj] === next) {
+            check = true
+            break
+          }
+        }
+        if (check) {
+          break
+        }
+      }
+      if (!check) {
+        errorNumber = next
+        break
+      }
+    }
+    var startIdx = 0
+    var endIdx = 0
+    var sum = 0
+    var n = 0
+    while (n < numbers.length && numbers[n] < errorNumber) {
+      if (sum > errorNumber) {
+        sum -= numbers[startIdx]
+        startIdx++
+      } else {
+        sum += numbers[n]
+        endIdx = n
+        n++
+      }
+      if (sum === errorNumber) {
+        break
+      }
+    }
+    var list = numbers.slice(startIdx,endIdx+1)
+    list.sort((a,b)=>{return a-b})
+    // console.log(list)
+    // console.log(list.reduce((acc,val)=>{
+    //   return acc + val
+    // }))
 
-    var result = 0
+    var result = list[0] + list[list.length-1]
     // console.log(result)
     $('#part2').append(input[i])
       .append('<br>&emsp;')
