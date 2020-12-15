d75f08fd35b6d3f72e2f66a1b121497e018482db
Lucas Rigo <lucas.rigo@sap.com>
2020-12-15 10:24:16 -0300

    day 14 part 2

--- day14/dockingData.js
+++ day14/dockingData.js
@@ -2,7 +2,11 @@ var input = [
 `mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
 mem[8] = 11
 mem[7] = 101
-mem[8] = 0` // 165
+mem[8] = 0` // 165 pt1
+,`mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1` // 208 // pt2
  ,puzzleInput
 ]
 
@@ -58,11 +62,61 @@ var part1 = function() {
 
 var part2 = function () {
 
-  for (var i = 0; i < input.length; i++) {
-    var numberStrings = input[i].split(/\s+/)
-    var numbers = $.map(numberStrings, (val => {return Number(val)}))
+  for (var i = 1; i < input.length; i++) {
+    var inputStrings = input[i].split(/\n+/)
+    var mask = ''
+    var memory = {}
+    for (var m = 0; m < inputStrings.length; m++) {
+      var line = inputStrings[m].split(/\s+/)
+      if (line[0] === 'mask') {
+        mask = line[2]
+      } else {
+        var memIdx = line[0].split(/\[|\]/)[1]
+        var val = Number(line[2])
+        var address = Number(memIdx)
+        var addressBinStr = (address >>> 0).toString(2).padStart(36,'0')
+        if (address.length>36) {
+          console.log('opaaa', val)
+        }
+        // apply mask to address
+        var newAddressBinStr = ''
+        for (var b = 0; b < mask.length; b++) {
+          if (mask.charAt(b) === '0') {
+            newAddressBinStr += addressBinStr.charAt(b)
+          } else { // 1 and X
+            newAddressBinStr += mask.charAt(b)
+          }
+        }
+        var xs = newAddressBinStr.split('').filter((x) => {
+          return x === 'X'
+        })
+        if (xs.length <= 0) {
+          console.log('opa',newAddressBinStr)
+        }
+        for (var x = 0; x < Math.pow(2,xs.length); x++) {
+          var combination = x.toString(2).padStart(xs.length,'0')
+          var c = 0
+          var combAddr = ''
+          for (var b = 0; b < newAddressBinStr.length; b++) {
+            if (newAddressBinStr.charAt(b) === 'X') {
+              combAddr += combination[c++]
+            } else {
+              combAddr += newAddressBinStr.charAt(b)
+            }
+          }
+          var numAddr = parseInt(combAddr,2)
+          memory[numAddr] = val
+        }
+      }
+    }
+    // console.log(memory)
 
-    var result = 0
+    // sum memory
+    var sum = 0
+    $.each(Object.keys(memory), (idx, key) =>{
+      sum += memory[key]
+    })
+    var result = sum
     // console.log(result)
     $('#part2').append(input[i])
       .append('<br>&emsp;')
