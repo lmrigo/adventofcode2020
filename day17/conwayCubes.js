b0406227d940e8cf139e6c57c655a4cf5835cd00
Lucas Rigo <lucas.rigo@sap.com>
2020-12-17 19:39:43 -0300

    day 17 part 2

--- day17/conwayCubes.js
+++ day17/conwayCubes.js
@@ -57,7 +57,6 @@ var part1 = function() {
       return adj
     }
 
-    //TODO: ajustar limites dos fors. por exemplo length + 1
     var cycle = function (i1,i2,j1,j2,k1,k2) {
       var newGrid = []
       for (var si = i1; si < i2; si++) {
@@ -142,11 +141,140 @@ var part1 = function() {
 var part2 = function () {
 
   for (var i = 0; i < input.length; i++) {
-    var numberStrings = input[i].split(/\s+/)
-    var numbers = $.map(numberStrings, (val => {return Number(val)}))
+    var cubeStrings = input[i].split(/\s+/)
 
-    var result = 0
-    // console.log(result)
+    var x1,x2,y1,y2,z1,z2,w1,w2
+    x1 = x2 = y1 = y2 = z1 = z2 = w1 = w2 = 0
+
+    var grid=[]
+    $.each(cubeStrings, (idx,line)=>{
+      var cells = line.split('')
+      grid[idx] = []
+      $.each(cells,(idx2,cell)=> {
+        if (cell === '#') {
+          grid[idx][idx2] = [['#']]
+          if (idx < x1) x1 = idx
+          if (idx > x2) x2 = idx
+          if (idx2 < y1) y1 = idx2
+          if (idx2 > y2) y2 = idx2
+        } else {
+          grid[idx][idx2] = [['.']]
+        }
+      })
+    })
+    // console.log(grid)
+    // console.log(x1,x2,y1,y2,z1,z2,w1,w2)
+
+    var calcAdj = function(ci,cj,ck,cl) {
+      var adj = ''
+       for (var ai = ci-1; ai <= ci+1; ai++) {
+        if (grid[ai] === undefined) {
+          grid[ai] = []
+        }
+        for (var aj = cj-1; aj <= cj+1; aj++) {
+          if (grid[ai][aj] === undefined) {
+            grid[ai][aj] = []
+          }
+          for (var ak = ck-1; ak <= ck+1; ak++) {
+            if (grid[ai][aj][ak] === undefined) {
+              grid[ai][aj][ak] = []
+            }
+            for (var al = cl-1; al <= cl+1; al++) {
+              if ((ai === ci) && (aj === cj) && (ak === ck) && (al === cl)) {
+                continue // skip the source position
+              }
+              if (grid[ai][aj][ak][al] !== undefined) {
+                adj += grid[ai][aj][ak][al]
+              } else {
+                adj += ''
+              }
+            }
+          }
+        }
+      }
+      return adj
+    }
+
+    var cycle = function (i1,i2,j1,j2,k1,k2,l1,l2) {
+      var newGrid = []
+      for (var si = i1; si < i2; si++) {
+        newGrid[si] = []
+        if (grid[si] === undefined) {
+          grid[si] = []
+        }
+        newGrid[si] = []
+        for (var sj = j1; sj < j2; sj++) {
+          newGrid[si][sj] = []
+          if (grid[si][sj] === undefined) {
+            grid[si][sj] = []
+          }
+          for (var sk = k1; sk < k2; sk++) {
+            newGrid[si][sj][sk] = []
+            if (grid[si][sj][sk] === undefined) {
+              grid[si][sj][sk] = []
+            }
+            for (var sl = l1; sl < l2; sl++) {
+              // console.log(si,sj,sk,sl)
+              var adjacency = calcAdj(si,sj,sk,sl)
+              var activeNeighbours = (adjacency.match(/#/g) || []).length
+              // If a cube is active
+              if (grid[si][sj][sk][sl] === '#') {
+                //  and exactly 2 or 3 of its neighbors are also active,
+                if (activeNeighbours === 2 || activeNeighbours === 3) {
+                  // the cube remains active.
+                  newGrid[si][sj][sk][sl] = '#'
+                } else {
+                  // Otherwise, the cube becomes inactive.
+                  newGrid[si][sj][sk][sl] = '.'
+                }
+              } else { // If a cube is inactive
+                // but exactly 3 of its neighbors are active,
+                if (activeNeighbours === 3) {
+                  // the cube becomes active.
+                  newGrid[si][sj][sk][sl] = '#'
+                } else {
+                  // Otherwise, the cube remains inactive.
+                  newGrid[si][sj][sk][sl] = '.'
+                }
+              }
+            }
+          }
+        }
+      }
+      return newGrid
+    }
+
+    var totalCycles = 6
+    var c = 0
+    while (c++ < totalCycles) {
+      grid = cycle(x1-c,x2+c+1,y1-c,y2+c+1,z1-c,z2+c+1,w1-c,w2+c+1)
+    }
+    // console.log(grid)
+
+    var printZ0 = function() {
+      var str = ''
+      for (var gi = x1-c+1; gi < grid.length; gi++) {
+        for (var gj = y1-c+1; gj < grid[gi].length; gj++) {
+          str += grid[gi][gj][0][0]
+        }
+        str += '\n'
+      }
+      console.log(str)
+    }
+    // printZ0()
+
+    var sum = 0
+    for (var gi = x1-c+1; gi < grid.length; gi++) {
+      for (var gj = y1-c+1; gj < grid[gi].length; gj++) {
+        for (var gk = z1-c+1; gk < grid[gi][gj].length; gk++) {
+          for (var gl = w1-c+1; gl < grid[gi][gj][gk].length; gl++) {
+            sum += (grid[gi][gj][gk][gl] === '#' ? 1 : 0)
+          }
+        }
+      }
+    }
+
+    var result = sum    // console.log(result)
     $('#part2').append(input[i])
       .append('<br>&emsp;')
       .append(result)
